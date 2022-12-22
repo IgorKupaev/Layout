e5f5c31e02f2689c0bc2f4f09b78b9b706246f7a
user <kupaev.itrum@gmail.com>
2022-12-22 17:24:56 +0300

    header - done

---
+++ scripts/burger-menu.js
@@ -0,0 +1,15 @@
+const menuOpen = document.querySelector("#menu-open");
+const menuClose = document.querySelector("#menu-close");
+const navList = document.querySelector("#nav-list");
+
+function openMenu() {
+    navList.style = "left: 0";
+}
+
+function closeMenu() {
+    navList.style = "left: -100%";
+}
+
+menuOpen.addEventListener('click', openMenu);
+menuClose.addEventListener('click', closeMenu);
+
